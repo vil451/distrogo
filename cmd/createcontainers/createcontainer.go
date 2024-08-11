6e10a871c326953b238be92c58e41e2a5c3b16b7
Sergey Raspopov <raspopov.s@mxm.email>
2024-08-11 23:58:38 +0300

    create container

--- cmd/createcontainers/createcontainer.go
+++ cmd/createcontainers/createcontainer.go
@@ -1,6 +1,16 @@
 package createcontainers
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"distrogo/cmd/dockerclient"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/spf13/cobra"
+	"io"
+	"os"
+)
 
 func CreateContainer() *cobra.Command {
 	var containerName string
@@ -11,7 +21,7 @@ func CreateContainer() *cobra.Command {
 		Short:   "Create a container",
 		Aliases: []string{"c"},
 		Run: func(cmd *cobra.Command, args []string) {
-			create(containerName)
+			create(containerName, pullImage)
 		},
 	}
 
@@ -41,6 +51,46 @@ func CreateContainer() *cobra.Command {
 	return command
 }
 
-func create(containerName string) {
+func create(containerName string, pull bool) {
+	ctx := context.Background()
+	cli, err := dockerclient.InitDockerClient()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating Docker client: %v\n", err)
+		os.Exit(1)
+	}
+	defer dockerclient.CloseDockerClient(cli)
+	if pull {
+		_, err := pullImage(ctx, cli, containerName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error pulling image: %v\n", err)
+		}
+	}
+	_, err = createContainer(ctx, cli, containerName)
+}
 
+func pullImage(ctx context.Context, cli *client.Client, name string) (io.ReadCloser, error) {
+	config := &container.Config{
+		Image: name,
+	}
+	resp, err := cli.ImagePull(ctx, config.Image, types.ImagePullOptions{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error pulling image: %v\n", err)
+		os.Exit(1)
+	}
+	return resp, nil
+}
+
+func createContainer(ctx context.Context, cli *client.Client, name string) (container.CreateResponse, error) {
+	//options := types.ContainerListOptions{}
+	config := &container.Config{
+		Image: name,
+		Cmd:   []string{"echo", "Hello, World!"},
+	}
+	hostConfig := &container.HostConfig{}
+	resp, err := cli.ContainerCreate(ctx, config, hostConfig, nil, nil, name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating container: %v\n", err)
+		os.Exit(1)
+	}
+	return resp, nil
 }
